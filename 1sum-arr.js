679a1d459c8c4b0a74d2b6715102b8c324d5f275
Silvia Momcheva <131309295+Silvia-Momcheva@users.noreply.github.com>
2025-03-12 18:19:18 +0200

    Create 1sum-arr.js

---
+++ 1sum-arr.js
@@ -0,0 +1,11 @@
+function sum(arr) {
+  let num1 = Number(input[0]);
+  let num2 = Number(input[1]);
+  let num3 = Number(input[2]);
+
+  let sum = num1 + num2 + num3;
+
+  console.log(sum);
+}
+
+sum(["2", "3", "4"]);
